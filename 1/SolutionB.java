7604737762369b073ffea8725e785ea671d948f3
WangLing <wangling@WangLingdeMacBook-Pro.local>
2018-01-01 21:11:06 +0800

    day 1

---
+++ 1/SolutionB.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class SolutionB {
+    //利用Map，时间复杂度为O(n)
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+               result[0] = map.get(target - nums[i]);
+                result[1] = i;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return result;
+    }
+}
