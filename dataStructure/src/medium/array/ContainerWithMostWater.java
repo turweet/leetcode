d0a8c513639851a42bf4b2953bd182b42469f2fd
wangLing <turweet@163.com>
2018-08-21 11:06:46 +0800

    daily

---
+++ dataStructure/src/medium/array/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package medium.array;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int left = 0;
+        int right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            if (area > max) {
+                max = area;
+            }
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
